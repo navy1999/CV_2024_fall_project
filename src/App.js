70b46ea638d89ace4000e2b4ecdda90096e0f2b0
Navneet Shankar <navneets099@gmail.com>
2024-10-08 19:36:31 -0400

    Abstract Page added along with Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
-import './App.css';
+import 'react-bootstrap'
 
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./pages/scripts/navbar"
+import About from "./pages/scripts/about"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+  <Router basename="/">
+    <Navbar/>
+    <Routes>
+      <Route exact path="/"  element={<About />} />
+    </Routes>
+  </Router>
+  </div> 
   );
 }
 
---
+++ src/pages/scripts/navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {NavDropdown,NavItem,Container,NavLink,ListGroup,ListGroupItem,Navbar,Nav, NavbarBrand} from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/navbar.css';
+import Scroll from 'react-scroll';
+import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+const ScrollLink = Scroll.ScrollLink
+const Topbar= () =>{
+  return (
+  <div>
+    <Navbar id="navbar-main" expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+    <Container>
+      <NavbarBrand href='/PersonalWebsite'>Computer Vision Project Proposal Fall 2024 -Virginia Tech</NavbarBrand>
+      <NavbarToggle aria-controls='basic-navbar-nav' />
+      <NavbarCollapse id='basic-navbar-nav'>
+        <Nav className='me-auto'>
+          <NavLink href="/PersonalWebsite/#/projects">Approach & Model</NavLink>
+        </Nav>
+      </NavbarCollapse>
+    </Container>
+    </Navbar>
+  </div>
+  );
+}
+export default Topbar;
